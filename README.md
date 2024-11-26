6dc4c2c9c8742e26f14a10a30a39166314b87592
Fabian Bull <spam+github@fbull.de>
2024-11-26 15:16:40 +0100

    Add chapter 1

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Apache Felix Tutorial
+
 Code to follow along the Apache Felix Tutorial
+
+## Chapter 1
+
+Start the Felix Shell by running `felix`.
+
+### Starting the bundle
+
+You can start the bundle by running:
+
+```sh
+start filet:example1/target/example1-1.0-SNAPSHOT.jar
+```
+
+Notice the logging statement.
+
+### Stopping the bundle
+
+You can also stop the bundle by running:
+
+```sh
+stop filet:example1/target/example1-1.0-SNAPSHOT.jar
+```
+
---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "A Nix-flake-based Java development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      javaVersion = 23; # Change this value to update the whole stack
+
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+      });
+    in
+    {
+      overlays.default =
+        final: prev:
+        let
+          jdk = prev."jdk${toString javaVersion}";
+        in
+        {
+          maven = prev.maven.override { jdk_headless = jdk; };
+          gradle = prev.gradle.override { java = jdk; };
+          lombok = prev.lombok.override { inherit jdk; };
+        };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; 
+          let felix_runner =             pkgs.writeShellScriptBin "felix" ''
+              java -jar ${pkgs.felix}/bin/felix.jar -b ${pkgs.felix}/bundle
+            '';
+          in [
+            gcc
+            felix
+            gradle
+            jdk
+            maven
+            ncurses
+            patchelf
+            zlib
+            felix_runner
+          ];
+
+          shellHook =
+            let
+              loadLombok = "-javaagent:${pkgs.lombok}/share/java/lombok.jar";
+              prev = "\${JAVA_TOOL_OPTIONS:+ $JAVA_TOOL_OPTIONS}";
+            in
+            ''
+              export JAVA_TOOL_OPTIONS="${loadLombok}${prev}"
+            '';
+        };
+      });
+    };
+}
